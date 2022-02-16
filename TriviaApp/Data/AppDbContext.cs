765154501572deb531a9d216aa2117e6918cdfb0
Misael <misa_30122@hotmail.com>
2022-02-15 21:16:55 -0800

    New Activity Component

--- TriviaApp/Data/AppDbContext.cs
+++ TriviaApp/Data/AppDbContext.cs
@@ -60,5 +60,6 @@ namespace TriviaApp.Data
         public DbSet<TriviaApp.Data.Entities.Comment> Comment { get; set; }
         public DbSet<TriviaApp.Data.Entities.Feeling> Feeling { get; set; }
         public DbSet<TriviaApp.Data.Entities.UserLike> UserLike { get; set; }
+        public DbSet<TriviaApp.Data.Entities.Activity> Activity { get; set; }
     }
 }
--- TriviaApp/Data/Entities/UserAnswers.cs
+++ TriviaApp/Data/Entities/UserAnswers.cs
@@ -1,20 +1,37 @@
-﻿namespace TriviaApp.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TriviaApp.Data.Entities
 {
     public class UserAnswers
     {
-        public UserAnswers(int userAnswersId, string question, string answer, bool isRight, int userScoreId)
+        public UserAnswers(int userAnswersId, string question, string answer, bool isRight, int userScoreId, string firstOption, string secondOption, string thirdOption, string fourthOption, string fifthOption, string sixthOption, string rightAnswer)
         {
             UserAnswersId = userAnswersId;
             Question = question;
             Answer = answer;
-            this.isRight = isRight;
+            IsRight = isRight;
             UserScoreId = userScoreId;
+            FirstOption = firstOption;
+            SecondOption = secondOption;
+            ThirdOption = thirdOption;
+            FourthOption = fourthOption;
+            FifthOption = fifthOption;
+            SixthOption = sixthOption;
+            RightAnswer = rightAnswer;
         }
 
+        [Key]
         public int UserAnswersId { get; set; }
         public string Question { get; set; }
         public string Answer { get; set; }
-        public Boolean isRight { get; set; }
+        public bool IsRight { get; set; }
         public int UserScoreId { get; set; }
+        public string FirstOption { get; set; }
+        public string SecondOption { get; set; }
+        public string ThirdOption { get; set; }
+        public string FourthOption { get; set; }
+        public string FifthOption { get; set; }
+        public string SixthOption { get; set; }
+        public string RightAnswer { get; set; }
     }
 }
